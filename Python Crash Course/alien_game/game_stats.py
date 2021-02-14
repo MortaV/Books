ae83d1918b37ee72833e26cdd37e7d5638525022
Morta Vilkaite <morta.vilkaite@gmail.com>
2021-02-14 13:51:48 +0200

    Added level

--- Python Crash Course/alien_game/game_stats.py
+++ Python Crash Course/alien_game/game_stats.py
@@ -9,7 +9,11 @@ class GameStats:
         # Start Alien Invasion in an active state
         self.game_active = False
 
+        # High scores not to reset
+        self.high_score = 0
+
     def reset_stats(self):
         """Initializa statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
--- Python Crash Course/alien_game/scoreboard.py
+++ Python Crash Course/alien_game/scoreboard.py
@@ -13,8 +13,10 @@ class Scoreboard:
         self.text_color = (26, 82, 118)
         self.font = pygame.font.SysFont('Montserrat', 25)
 
-        # Prepare the initial score image
+        # Prepare the initial score images
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Turn the score into a rendered image"""
@@ -30,6 +32,40 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 2
 
+    def prep_high_score(self):
+        """Turn the high score into a rendered image"""
+        high_score = round(self.stats.higg_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(
+            high_score_str, True,
+            self.text_color, self.settings.bg_color
+        )
+
+        # Center the high score
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def prep_level(self):
+        """Turn a level into a rendered image"""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(
+            level_str, True, self.text_color, self.settings.bg_color
+        )
+
+        # position level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 2
+
     def show_score(self):
-        """Draw score on the screen"""
+        """Draw score and level on the screen"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+
+    def check_high_score(self):
+        """Check to see if there's a new high score"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
