2ba5b8da40bcba79ad120a00ed8c3f920f9af458
Morta Vilkaite <morta.vilkaite@gmail.com>
2021-02-14 13:57:26 +0200

    Fixed level

--- Python Crash Course/alien_game/scoreboard.py
+++ Python Crash Course/alien_game/scoreboard.py
@@ -34,7 +34,7 @@ class Scoreboard:
 
     def prep_high_score(self):
         """Turn the high score into a rendered image"""
-        high_score = round(self.stats.higg_score, -1)
+        high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
         self.high_score_image = self.font.render(
             high_score_str, True,
